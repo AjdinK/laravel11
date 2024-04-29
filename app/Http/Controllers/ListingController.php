1759bb6f55ab9905fb871b99d26b76596f2c0778
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-04-29 12:15:24 +0200

    update

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,22 +9,32 @@ class ListingController extends Controller
 {
     //to show all listings
     public function index() {
+
+        // dd(request('tag'));
+
         return view('listings.index', [
-            'listings' => Listing::All()
+            'listings' => Listing::latest()
+            ->filter(request(['tag','search']))
+            ->get()
         ]);
+
     }
 
     //to show single listing
     public function show (int $id) {
+
         $data = Listing::find($id);
+
         if ($data){
             return view ('listings.show',[
                 'listing' => $data
-            ]);}
+            ]);
+        }
 
         else {
             abort('404');
         }
+
     }
 
     //to show single listing
