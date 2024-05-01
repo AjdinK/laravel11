a199a25a38a12a4a79751999a37833076d43fd01
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-05-01 23:05:40 +0200

    update

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title','company','email','tags','description','location','website',];
+
     public function scopeFilter ($query , array $filters) {
         if ($filters['tags']?? false) {
             $query->where ('tags','like','%'.request('tags').'%');
